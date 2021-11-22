4ece71be4eb63859a4f181aa57f534e7d809ba67
Nguyễn Trần Duy Thức ITITIU20318 <thuccau1072002@gmail.com>
2021-11-22 14:42:31 +0700

    bai1

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -10,8 +10,73 @@
 #include <stdlib.h>
 #include <math.h>
 
+
 void Ex1(int n){
 	//Your codes here
+	int i, a[4];
+
+ if (n > 999 && n < 10000)
+ {
+  printf("");
+  for (i = 0; i < 4; i++)
+  {
+   a[i] = n % 10;
+   n = n / 10;
+  }
+ switch (a[3])
+  {
+  case 1:printf(" one"); break;
+  case 2:printf(" two"); break;
+  case 3:printf(" three"); break;
+  case 4:printf(" four"); break;
+  case 5:printf(" five"); break;
+  case 6:printf(" six"); break;
+  case 7:printf(" seven "); break;
+  case 8:printf(" eight "); break;
+  case 9:printf(" nine "); break;
+  }
+  printf(" thousand");
+  switch (a[2])
+  {
+  case 1:printf(" one"); break;
+  case 2:printf(" two"); break;
+  case 3:printf(" three"); break;
+  case 4:printf(" four"); break;
+  case 5:printf(" five"); break;
+  case 6:printf(" six"); break;
+  case 7:printf(" seven"); break;
+  case 8:printf(" eight"); break;
+  case 9:printf(" nine"); break;
+  case 0:printf(" zero"); break;
+  }
+  printf(" hundred");
+  switch (a[1])
+  {
+  case 1:printf(" ten"); break;
+  case 2:printf(" twenty"); break;
+  case 3:printf(" thirty"); break;
+  case 4:printf(" forty"); break;
+  case 5:printf(" fifty"); break;
+  case 6:printf(" sixty"); break;
+  case 7:printf(" seventy"); break;
+  case 8:printf(" eighty"); break;
+  case 9:printf(" ninety"); break;
+  case 0:printf(" odd"); break;
+  }
+  switch (a[0])
+  {
+  case 1:printf(" one"); break;
+  case 2:printf(" two"); break;
+  case 3:printf(" three"); break;
+  case 4:printf(" four"); break;
+  case 5:if (a[1] == 0) printf(" five");
+      
+  case 6:printf(" six"); break;
+  case 7:printf(" seven "); break;
+  case 8:printf(" eight "); break;
+  case 9:printf(" nine "); break;
+  }
+ }
 	
 }
 
