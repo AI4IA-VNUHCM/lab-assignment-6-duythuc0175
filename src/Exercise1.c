dfdbaec04fa7079c2fd69236a5caa2bb596d3e36
Nguyễn Trần Duy Thức ITITIU20318 <thuccau1072002@gmail.com>
2021-11-22 14:45:36 +0700

    bai1

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -31,9 +31,9 @@ void Ex1(int n){
   case 4:printf(" four"); break;
   case 5:printf(" five"); break;
   case 6:printf(" six"); break;
-  case 7:printf(" seven "); break;
-  case 8:printf(" eight "); break;
-  case 9:printf(" nine "); break;
+  case 7:printf(" seven"); break;
+  case 8:printf(" eight"); break;
+  case 9:printf(" nine"); break;
   }
   printf(" thousand");
   switch (a[2])
